b84017fcaf0190ef748f744f2fd2a93bf3a6d516
Ondrej Zika <zika.ondra@gmail.com>
2021-02-02 19:10:36 +0100

    fixed a bug

--- scripts/nsi.py
+++ scripts/nsi.py
@@ -4,12 +4,6 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-## create requirements.txt
-create=True
-if create:
-    text_file = open("requirements.txt", "w")
-    text_file.write('\n'.join(f'{m.__name__} {m.__version__}' for m in globals().values() if getattr(m, '__version__', None)))
-    text_file.close()
 
 
 sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
@@ -24,39 +18,49 @@ df=df.set_index("date")
 vars = ["st", "wl", "ml", "gm"]
 labels = ["Stress", "Workload", "Motivation", "Mood"]
 
-
-mainvar = "Stress"
-v = vars[labels.index(mainvar)]
-
-tdf = df.filter(regex=v+'_\d') # I love regex
-tdf["date"] = tdf.index
-tdf = pd.wide_to_long(tdf, stubnames=v+"_", i="date", j="score").reset_index().rename(columns={v+"_": v}).dropna()
-tdf = tdf.loc[tdf.index.repeat(tdf[v])].reset_index()
-tdf["date"] = pd.to_datetime(tdf["date"], format='%d/%m/%Y')
-tdf["date"] = tdf["date"].dt.strftime('%Y-%m-%d')
-tdf = tdf.sort_values(by='date',ascending=False)
-
-
-# Initialize the FacetGrid object
-pal = sns.cubehelix_palette(6, rot=-.25, light=.7)
-g = sns.FacetGrid(tdf, row="date", hue="date", aspect=4, height=1.8, palette=pal)
-g.map(sns.kdeplot, "score",
-  bw_adjust=.5, clip_on=False,
-  fill=True, alpha=1, linewidth=1.5)
-g.map(sns.kdeplot, "score", clip_on=False, color="k", lw=2, bw_adjust=.5)
-g.map(plt.axhline, y=0, lw=2, clip_on=False)
-g.map(label, "score")
-g.fig.subplots_adjust(hspace=-.35)
-g.set_titles("")
-g.set(yticks=[])
-g.despine(bottom=True, left=True)
-
-
+pltype = "Wavy"
+mainvar = "Workload"
 st.sidebar.markdown('## Side menu')
 mainvar = st.sidebar.selectbox(
     'Which variable would you like to see',
     ('Stress', 'Workload', 'Motivation', 'Mood')
 )
 
+pltype = st.sidebar.selectbox(
+    'Plot type',
+    ('Wavy', 'Simple')
+)
+
 st.title("Neurocode mental health index: "+mainvar)
+
+v = vars[labels.index(mainvar)]
+
+tdf = df.filter(regex=vars[labels.index(mainvar)]+'_\d') # I love regex
+tdf["date"] = tdf.index
+tdf = pd.wide_to_long(tdf, stubnames=vars[labels.index(mainvar)]+"_", i="date", j="score").reset_index().rename(columns={vars[labels.index(mainvar)]+"_": vars[labels.index(mainvar)]}).dropna()
+tdf = tdf.loc[tdf.index.repeat(tdf[vars[labels.index(mainvar)]])].reset_index()
+tdf["date"] = pd.to_datetime(tdf["date"], format='%d/%m/%Y')
+tdf["date"] = tdf["date"].dt.strftime('%Y-%m-%d')
+tdf = tdf.sort_values(by='date',ascending=False)
+
+
+if pltype == "Wavy":
+    # Initialize the FacetGrid object
+    pal = sns.cubehelix_palette(6, rot=-.25, light=.7)
+    g = sns.FacetGrid(tdf, row="date", hue="date", aspect=4, height=1.8, palette=pal)
+    g.map(sns.kdeplot, "score",
+      bw_adjust=.5, clip_on=False,
+      fill=True, alpha=1, linewidth=1.5)
+    g.map(sns.kdeplot, "score", clip_on=False, color="k", lw=2, bw_adjust=.5)
+    g.map(plt.axhline, y=0, lw=2, clip_on=False)
+    g.map(label, "score")
+    g.fig.subplots_adjust(hspace=-.35)
+    g.set_titles("")
+    g.set(yticks=[])
+    g.despine(bottom=True, left=True)
+elif pltype == "Simple":
+    a = 1
+
+plt.show()
+
 st.pyplot(g)
